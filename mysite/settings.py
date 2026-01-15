3b7da23947797968ab48632f11d2b9802c82e4c6
dastanslam <dslam5956@gmail.com>
2026-01-15 11:58:52 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,3 +139,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
